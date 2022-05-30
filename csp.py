ec3d1cfa65afb1d1c251d12ea171de1568d39654
1701490 <1701490@eng.asu.edu.eg>
2022-05-30 23:37:52 +0200

    initalize csp class

--- csp.py
+++ csp.py
@@ -19,3 +19,25 @@ class utilities():
         return any(x is elt for x in seq)
 
 
+# ...................
+class CSP(Problem):
+
+    def __init__(self, variables, domains, neighbors, constraints):
+        variables = variables or list(domains.keys())
+
+        self.variables = variables
+        self.domains = domains
+        self.neighbors = neighbors
+        self.constraints = constraints
+        self.curr_domains = None
+        self.nassigns = 0
+
+    def assign(self, var, val, assignment):
+        assignment[var] = val
+        self.nassigns += 1
+
+    def unassign(self, var, assignment):
+        if var in assignment:
+            del assignment[var]
+
+
