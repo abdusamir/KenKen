ca59d193bbf7611bfaea01640ce72e0b04810b92
Menna Azaban <mennaazaban2034@gmail.com>
2022-05-31 21:48:57 +0200

    Creating problem class

--- csp.py
+++ csp.py
@@ -18,7 +18,30 @@ class utilities():
     def is_in(elt, seq):
         return any(x is elt for x in seq)
 
+class Problem(object):
 
+    def __init__(self, initial, goal=None):
+        self.initial = initial
+        self.goal = goal
+
+    def actions(self, state):
+        raise NotImplementedError
+
+    def result(self, state, action):
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        if isinstance(self.goal, list):
+            return utilities.is_in(state, self.goal)
+        else:
+            return state == self.goal
+
+    def path_cost(self, c, state1, action, state2):
+        return c + 1
+
+    def value(self, state):
+        raise NotImplementedError
+        
 # ...................
 class CSP(Problem):
 
