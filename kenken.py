fa83539fb7bc8809f1df22248f0af6144a68e86d
Abdelrahman Ahmed <73047153+Sameerjr1@users.noreply.github.com>
2022-05-31 13:49:48 +0200

    add adjacent function

--- kenken.py
+++ kenken.py
@@ -20,7 +20,11 @@ class generateKenkenPuzzle():
         else:
             return None
 
-    
+    def adjacent(xy1, xy2):
+        x1, y1 = xy1
+        x2, y2 = xy2
+        dx, dy = x1 - x2, y1 - y2
+        return (dx == 0 and abs(dy) == 1) or (dy == 0 and abs(dx) == 1)
 
     
     
