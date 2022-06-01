e2bad7f696a3fbc738f6b9569dcf9de0d588657c
zeyadmourad99 <74146582+zeyadmourad99@users.noreply.github.com>
2022-06-01 21:25:00 +0200

    implement loading component

---
+++ Front-End/src/Loading.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactLoading from "react-loading";
+
+const Lodaing = () => (
+  <div>
+    <ReactLoading
+      color={"#000"}
+      type={"spin"}
+      height={"120px"}
+      width={"120px"}
+    />
+  </div>
+);
+
+export default Lodaing;
