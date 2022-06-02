b5f39e0af7277697dce169cc5cba6e70a6b29fcc
Menna Azaban <mennaazaban2034@gmail.com>
2022-06-02 15:58:21 +0200

    modify purne

--- Back-End/csp.py
+++ Back-End/csp.py
@@ -96,5 +96,5 @@ class CSP(Problem):
 
     def prune(self, var, value, removals):
         self.curr_domains[var].remove(value)
-        if removals is None:
+        if removals is not None:
             removals.append((var, value))
